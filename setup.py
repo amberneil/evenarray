e3ec711c273ef30b9e4908aa2998d780e6eb1228
Neil Hansen <neil@amber.film>
2019-01-21 10:49:13 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name='evenarray',
+        version='0.1.0',
+        description='Creates even array of intergers or floats.',
+        author='Neil Hansen',
+        package_dir={'evenarray': 'evenarray'},
+        author_email='neil@amber.film',
+        packages=['evenarray'],
+    )
